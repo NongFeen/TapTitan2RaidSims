cef73be8884b11b8dc9e93505c34c273c44d4cfb
NongFeen <kakfeenza@gmail.com>
2026-06-23 17:11:22 +0700

    add : razorwind

--- src/services/taptitan/card_function/burst/razor_wind.rs
+++ src/services/taptitan/card_function/burst/razor_wind.rs
@@ -1,14 +1,21 @@
 use crate::models::{
-    boss::{Boss, BossPartName, PartState},
-    cards::Card,
+    boss::{Boss, BossPartName, PartState}, card_skill_data::{card_skill_bonusamountC, card_skill_value_a}, cards::Card,
 };
 
+pub fn get_proc_chance(_card: &Card, _boss: &Boss) -> f64 {
+    0.12
+}
+
 pub fn on_proc(
-    _card: &Card,
+    card: &Card,
     boss: &mut Boss,
     target_part: BossPartName,
     damage: f64,
 ) -> f64 {
-    let can_hit = matches!(boss.part(target_part).part_state, PartState::Body | PartState::Skeleton);
-    if can_hit { damage * 1.5 } else { damage }
+    let razor_wind_mult = card_skill_value_a(card.card_id, card.level).unwrap_or(1.0);
+    let mut total_mult = razor_wind_mult;
+    if(boss.get_state_from_part(target_part) == PartState::Body){
+        total_mult *= card_skill_bonusamountC(card.card_id).unwrap_or(1.0);
+    }
+    return  damage * total_mult;
 }
