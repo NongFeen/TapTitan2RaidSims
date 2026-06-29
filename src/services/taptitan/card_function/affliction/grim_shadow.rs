a0dce56e12bb46a70699baa210d1e8a64cf9b887
NongFeen <kakfeenza@gmail.com>
2026-06-29 22:55:16 +0700

    add : grim_shadow

--- src/services/taptitan/card_function/affliction/grim_shadow.rs
+++ src/services/taptitan/card_function/affliction/grim_shadow.rs
@@ -7,10 +7,11 @@ use crate::models::{
 
 use super::shared;
 
-const TICK_INTERVAL_SECONDS: f64 = 1.0;
+const TICK_INTERVAL_SECONDS: f64 = 0.2;
 
 pub fn get_proc_chance(card: &Card, boss: &Boss) -> f64 {
-    shared::get_proc_chance(card, boss)
+    // shared::get_proc_chance(card, boss)
+    1.0
 }
 pub fn on_proc(card: &Card, boss: &mut Boss, target_part: BossPartName, damage: f64) {
     shared::on_proc_with_tick_interval(card, boss, target_part, damage, TICK_INTERVAL_SECONDS)
@@ -25,9 +26,10 @@ pub fn on_tick(
 ) -> u64 {
     let stack_mult = card_skill_bonusamountC(affliction.source_card).unwrap_or(1.2);
     let multiplier =
-        stack_multiplier * stack_mult.powi(affliction.stack_count().saturating_sub(1) as i32);
+        stack_multiplier * stack_mult.powf(affliction.stack_count() as f64);
+
     shared::on_tick(affliction, boss, part_name, multiplier, elapsed_seconds)
-}
+}//3.16 / 7.59
 
 pub fn on_remove(affliction: &Affliction, attached_duration: f64) -> u64 {
     shared::on_remove(affliction, attached_duration)
